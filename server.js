f859ef626477fb03e7740130decc2859c8931299
Patrick <patasax22@yahoo.com>
2022-01-11 20:37:39 -0500

    all rooms route added

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 
 const dbConfig = require('./db');
+const roomsRoute = require('./routes/roomsRoute');
 
 
+app.use('/api/rooms', roomsRoute);
 
 const port = process.env.PORT || 5000;
 
