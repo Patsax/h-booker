93061473913e6dd8f7fddb6260be3be72d484c74
Patrick <patasax22@yahoo.com>
2022-01-13 20:17:23 -0500

    server adjustment and module installs

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const db = require('./db');
 
-const roomsRoute = require('./routes/roomsRoute');
-const userRoute = require('./routes/bookingsRoute');
 const bookingsRoute=require('./routes/bookingsRoute');
+const roomsRoute = require('./routes/roomsRoute');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 
@@ -13,6 +13,17 @@ app.use(express.json());
 app.use('/api/rooms', roomsRoute);
 app.use('/api/users' , userRoute);
 app.use('/api/bookings' , bookingsRoute);
+
+if(process.env.NODE_ENV ==='production')
+{
+    app.use('/' , express.static('client/build'))
+
+    app.get('*' , (req , res)=>{
+
+        res.sendFile(path.resolve(__dirname  , 'client/build/index.html'))
+
+    })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port} using nodemon`));
