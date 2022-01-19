02ac7f22df3b71adf7e522774b4393049dd1bf12
Patrick <patasax22@yahoo.com>
2022-01-18 19:31:23 -0500

    gitignore added

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ if(process.env.NODE_ENV ==='production')
     })
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3005;
 app.listen(port, () => console.log(`Server running on port ${port} using nodemon`));
